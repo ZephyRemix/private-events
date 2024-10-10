97c1dea1b7bb86477537164bb6cca9f93bc7273a
ZephyRemix <hunyaochong@gmail.com>
2024-10-10 09:36:47 +0800

    Added feature: user creating events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,24 @@ class EventsController < ApplicationController
   def index
     @event = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events_created.new(event_params)
+    if @event.save
+      flash[:success] = "Object successfully created"
+      redirect_to root_path
+    else
+      flash[:error] = "Something went wrong"
+      render "new"
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :date, :location)
+  end
 end
